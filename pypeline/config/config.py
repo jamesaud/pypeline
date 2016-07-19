7f1f29a01530f35bb61e9a81b83afa2e4c7bfb03
Audretsch <ja048043@m1413885.northamerica.cerner.net>
2016-07-19 17:46:37 -0500

    small changes

--- pypeline/config/config.py
+++ pypeline/config/config.py
@@ -7,7 +7,7 @@ import docker.tls as tls
 Set configuration in init.py.
 """
 
-DOCKER_MACHINE_IP = '192.168.99.100'
+DOCKER_MACHINE_IP = None
 CLIENT = None
 
 
@@ -16,16 +16,14 @@ def set_logger():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 
-def setup():
+def setup(docker_machine_ip='192.168.99.100'):
     global CLIENT
     global DOCKER_MACHINE_IP
-    if CLIENT is None:
-        CERTS = os.path.join(os.path.expanduser('~'), '.docker', 'machine', 'machines', 'default')
-        TLS_CONFIG = tls.TLSConfig(
-            client_cert=(os.path.join(CERTS, 'cert.pem'), os.path.join(CERTS, 'key.pem')),
-            ca_cert=os.path.join(CERTS, 'ca.pem'),
-            verify=True)
+    DOCKER_MACHINE_IP = docker_machine_ip
+    CERTS = os.path.join(os.path.expanduser('~'), '.docker', 'machine', 'machines', 'default')
+    TLS_CONFIG = tls.TLSConfig(
+        client_cert=(os.path.join(CERTS, 'cert.pem'), os.path.join(CERTS, 'key.pem')),
+        ca_cert=os.path.join(CERTS, 'ca.pem'),
+        verify=True)
 
-        CLIENT = docker.Client(base_url='https://{}:2376'.format(DOCKER_MACHINE_IP), tls=TLS_CONFIG)
-
-        # DOCKER_CLIENT = docker.from_env(assert_hostname=False)
+    CLIENT = docker.Client(base_url='https://{}:2376'.format(DOCKER_MACHINE_IP), tls=TLS_CONFIG)
