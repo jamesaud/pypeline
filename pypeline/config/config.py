26eff9d332723162cfaadb2e61f9404761ca62a8
Audretsch <ja048043@m1413885.northamerica.cerner.net>
2016-07-20 14:38:41 -0500

    ability to set docker client

--- pypeline/config/config.py
+++ pypeline/config/config.py
@@ -1,13 +1,12 @@
 import docker
+import docker.tls as docker_tls
 import os
 import logging
-import docker.tls as tls
 
 """
 Set configuration in init.py.
 """
 
-DOCKER_MACHINE_IP = None
 CLIENT = None
 DEFAULT_REGISTRY = 'https://index.docker.io/v1/'
 
@@ -17,14 +16,31 @@ def _set_logger():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 
-def setup(docker_machine_ip='192.168.99.100'):
+def setup(DOCKER_BASE_URL='https://192.168.99.100:2376',
+          CERTS=os.path.join(os.path.expanduser('~'), '.docker', 'machine', 'machines', 'default'),
+          TLS=None):
+    """
+    Setup the docker machine.
+    :param docker_base_url: Str - url of the docker machine
+    :param certs_path: Str - path of the certs.
+    :param TLS: Dict: required keys: {client_cert: Str, ca_cert: Str, verify: Bool} - custom keys.
+    :return:
+    - note - refer to the dockerpy documentation.
+    """
     global CLIENT
-    global DOCKER_MACHINE_IP
-    DOCKER_MACHINE_IP = docker_machine_ip
-    CERTS = os.path.join(os.path.expanduser('~'), '.docker', 'machine', 'machines', 'default')
-    TLS_CONFIG = tls.TLSConfig(
-        client_cert=(os.path.join(CERTS, 'cert.pem'), os.path.join(CERTS, 'key.pem')),
-        ca_cert=os.path.join(CERTS, 'ca.pem'),
-        verify=True)
 
-    CLIENT = docker.Client(base_url='https://{}:2376'.format(DOCKER_MACHINE_IP), tls=TLS_CONFIG)
+    if TLS is None:
+        client_cert = os.path.join(CERTS, 'cert.pem'), os.path.join(CERTS, 'key.pem')
+        ca_cert = os.path.join(CERTS, 'ca.pem')
+        verify = True
+    else:
+        client_cert = TLS['client_cert']
+        ca_cert = TLS['ca_cert']
+        verify = TLS['verify']
+
+    TLS_CONFIG = docker_tls.TLSConfig(
+        client_cert=client_cert,
+        ca_cert=ca_cert,
+        verify=verify)
+
+    CLIENT = docker.Client(base_url=DOCKER_BASE_URL, tls=TLS_CONFIG)
