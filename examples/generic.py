b2e6ba0ea0fddd7dad5d4957ed184294dfb7e413
jamesaud <jamesaud@gmail.com>
2016-08-09 15:51:04 -0400

    Update generic.py

--- examples/generic.py
+++ examples/generic.py
@@ -33,28 +33,7 @@ def test():
         GP.push(tag_with_time())  # Tag before it pushes, defaults to latest with no argument
 
 
-def test2():
-    """
-    Using a rails app.
-    Alternative syntax. If you forget to close, the git cloned work directory will be left in your current directory.
-    """
-    clientsetup(default=True, docker_base_url='https://192.168.99.100:2376')
-
-    RP = GenericPipeline()
-    try:
-        RP.login(registry='dockerhub.cerner.com/', repository='jamesaudretsch')
-        stage("Clone and Build")
-        RP.build('https://github.cerner.com/JA048043/r_wellness')
-        stage("Test")
-        RP.test('rspec spec', 'rspec spec', 'echo "third test"')  # Run parallel tests in separate containers
-        stage("Push")
-        RP.push('latest')
-    finally:  # Make sure to cleanup on an error. 'with' syntax is preferred.
-        RP.close()
-
 
 if __name__ == "__main__":
     print('\n\n{}\nEXAMPLE 1 -------------\n{}'.format('-' * 23, '-' * 23))
     test()
-    print('\n\n{}\nEXAMPLE 2 -------------\n{}'.format('-' * 23, '-' * 23))
-    test2()
